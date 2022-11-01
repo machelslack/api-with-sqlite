4694fe1101cf38497469a1795f95b521630f26b6
machelslackatnewsuk <machelsandile.slack@news.co.uk>
2022-11-01 10:58:08 +0000

    added authentication middleware

---
+++ staff/middleware/authentication.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const authenticateToken = (req, res, next) => {
+  const token = req.headers.token;
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    console.log(err);
+
+    if (err) return res.sendStatus(403);
+
+    req.user = user;
+
+    next();
+  });
+};
+
+module.exports = authenticateToken;
---
+++ staff/middleware/authentication.test.js
@@ -0,0 +1,43 @@
+const authenticateToken = require("./authentication");
+const jwt = require("jsonwebtoken");
+
+const jwtverifyMock = jest.fn().mockReturnedValue("a-token");
+
+const mocks = {
+  requestMock: {
+    body: {
+      name: "staff-name",
+    },
+    method: "POST",
+    headers: {
+      token: "mock-token",
+    },
+  },
+  responseMock: {
+    status: jest.fn(),
+    set: jest.fn(),
+    json: jest.fn(),
+  },
+  nextMock: jest.fn(),
+  jwtMockCallback: (err, user) => {},
+  TOKEN_SECRET: "xxx",
+  username: "johndoe",
+  expiry: { expiresIn: "1800s" },
+};
+
+let verifyMock = jest.spyOn(jwt, "verify");
+
+jest.mock("jwtverifyMock", () => ({
+  default: {
+    verify: jest.fn(),
+  },
+}));
+
+describe.skip("Authentication Middleware", () => {
+  beforeAll(() => {
+    process.env.TOKEN_SECRET = mocks.TOKEN_SECRET;
+  });
+  it("should return sucess response message", () => {
+    authenticateToken(mocks.requestMock, mocks.responseMock, mocks.nextMock);
+  });
+});
